5a5f96ab3332c083acf4aed8ec52827fea1a9d56
Mariana <73724268+golangis@users.noreply.github.com>
2021-03-02 19:48:12 +0000

    Create ascii_value.cpp

---
+++ veryBasicExercises/ascii_value.cpp
@@ -0,0 +1,11 @@
+//ex 1.1
+#include <iostream>
+using namespace std;
+char car;
+int main() {
+	cout << "This is the question number 1.1! \n";
+	cout << "Type a char: \n";
+	cin >> car;
+	cout << "The ASCII number of the char '" << car << "' is: " << int(car)<<".";
+	return 0;
+}
